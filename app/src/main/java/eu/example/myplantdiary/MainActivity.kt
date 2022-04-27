12c9c1ece0dd4ac8bf7852d70a6b3fa756ee258b
Michael Christensen <mcct@privat.dk>
2022-04-27 15:52:18 +0200

    Deleted function call to greetings in MainActivity

--- app/src/main/java/eu/example/myplantdiary/MainActivity.kt
+++ app/src/main/java/eu/example/myplantdiary/MainActivity.kt
@@ -29,22 +29,22 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colors.background
 				) {
-					Greeting("Android")
+
 				}
 			}
 		}
 	}
 }
 
-@Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
-}
+//@Composable
+//fun Greeting(name: String) {
+//	Text(text = "Hello $name!")
+//}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
 	MyPlantDiaryTheme {
-		Greeting("Android")
+		// Greeting("Android")
 	}
 }
